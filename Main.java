ddf6e11623cc811fea53895d1fe2b24b224b3837
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-01-16 19:20:29 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+public class Main {  
+  public static void main(String[] args) {
+    MyFrame myFrame = new MyFrame();
+  }
+}
---
+++ MyFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+
+  DragPanel dragPanel = new DragPanel();
+  
+  MyFrame(){
+    this.add(dragPanel);
+    this.setTitle("Drag & Drog");
+    this.setSize(600, 600);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setVisible(true);
+  }
+}
